21f719d1dd928f492863192bd9fa966a77e7246b
Tim Shaker <tim@tmshkr.com>
2024-05-04 21:45:26 -0700

    task 2 progress

--- src/main/java/StudentDirectory.java
+++ src/main/java/StudentDirectory.java
@@ -1,14 +1,105 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentDirectory {
 	double[][] data;
+	ArrayList<String> studentNames;
+	ArrayList<Integer> studentGrades;
 
 	public StudentDirectory(int numStudents, int numAssignments) {
 		this.data = new double[numStudents][numAssignments];
+		this.studentNames = new ArrayList<String>();
+		this.studentGrades = new ArrayList<Integer>();
 	}
 
 	public static void main(String[] args) {
-		StudentDirectory directory = new StudentDirectory(3, 3);
-		populateData(directory.data);
-		printData(directory.data);
+		Scanner in = new Scanner(System.in);
+		StudentDirectory dir = new StudentDirectory(3, 3);
+		boolean exit = false;
+
+		while (!exit) {
+			printMenu();
+			int option = in.nextInt();
+			switch (option) {
+				case 1:
+					dir.addStudent();
+					break;
+				case 2:
+					dir.removeStudent();
+					break;
+				case 3:
+					dir.listStudent();
+					break;
+				case 4:
+					dir.printDeansList();
+					break;
+				case 5:
+					exit = true;
+					break;
+
+				default:
+					break;
+			}
+		}
+
+		System.out.println("Exiting the program. Goodbye!");
+
+		in.close();
+	}
+
+	public static void printMenu() {
+		System.out.println("Orange Coast College Student Directory");
+		System.out.println("--------------------------------------");
+		System.out.println("1. Add Student and Grade");
+		System.out.println("2. Remove Student");
+		System.out.println("3. List Students");
+		System.out.println("4. Dean's List");
+		System.out.println("5. Exit");
+		System.out.print("Select an option: ");
+	}
+
+	public void addStudent() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the student's name: ");
+		String name = in.nextLine();
+		studentNames.add(name);
+
+		System.out.print("Enter the student's grade: ");
+		int grade = in.nextInt();
+		studentGrades.add(grade);
+		System.out.println(name + " with grade " + grade + " added to the list.");
+
+	}
+
+	public void removeStudent() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter the student's name to remove: ");
+		String name = in.nextLine();
+		int index = studentNames.indexOf(name);
+		if (index == -1) {
+			System.out.println(name + " was not found in the list.");
+		} else {
+			studentNames.remove(index);
+			studentGrades.remove(index);
+			System.out.println(name + " has been removed from the list.");
+		}
+
+	}
+
+	public void listStudent() {
+		System.out.println("Student List:");
+		for (int i = 0; i < studentNames.size(); i++) {
+			System.out.println(studentNames.get(i) + " - Grade: " + studentGrades.get(i));
+		}
+	}
+
+	public void printDeansList() {
+		System.out.println("Dean's List:");
+		for (int i = 0; i < studentNames.size(); i++) {
+			if (studentGrades.get(i) >= 90) {
+				System.out.println(studentNames.get(i) + " - Grade: " + studentGrades.get(i));
+			}
+		}
 	}
 
 	public static void printData(double[][] data) {
